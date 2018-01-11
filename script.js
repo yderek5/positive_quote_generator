e5fb6ae33c2630706f82c295efb3bdee8f975b16
yderek5 <yderek5@gmail.com>
2018-01-11 16:46:19 -0600

    fix bug

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
       method: "GET"
     }).done(function(response) {
       var post = response.shift();
+      var quoteArray = [];
       var randNum = Math.floor(Math.random() * response.length);
       console.log(response[randNum]);
       $("#quote").append(response[randNum].content);
